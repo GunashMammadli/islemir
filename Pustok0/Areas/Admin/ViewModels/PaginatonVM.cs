be984dcc2d4962daad75aa7f2d3a7f9c51bf2276
GunashMammadli <mi7ehrg0p@code.edu.az>
2023-12-13 12:25:09 +0300

    cehenneme

---
+++ Pustok0/Areas/Admin/ViewModels/PaginatonVM.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace Pustok0.Areas.Admin.ViewModels
+{
+    public class PaginatonVM<T> where T : IEnumerable
+    {
+        public int TotalCount { get; }
+        public T Items { get; }
+        public PaginatonVM(int totalCount, T items)
+        {
+            TotalCount = totalCount;
+            Items = items;
+        }
+    }
+}
+
